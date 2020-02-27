9d1a5e9c1053a4d4229e1bd87528ce527c7d6291
Mark T. Holder <mtholder@gmail.com>
2020-02-27 12:38:37 -0600

    testing get_tree via the object format

---
+++ testopentree/test_get_tree.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import unittest
+from testopentree import run_example_script_or_skip as ex_runner
+
+class TestExamples(unittest.TestCase):
+
+    def test_get_tree(self):
+        obj = {
+            "matched_trees": [
+                {"ot:studyId": "pg_719", "ot:treeId": "tree1294"},
+                {"ot:studyId": "pg_719", "ot:treeId": "tree1295"},
+                {"ot:studyId": "pg_719", "ot:treeId": "tree1296"}
+            ],
+            "ot:studyId": "pg_719"
+            }
+        res = {"matched_studies": [obj]}
+
+        def res_checker(test_case, results):
+            test_case.assertTrue(str(results.tree).startswith('('))
+
+        args = ['ot_1877', 'tree3', '--format=object']
+        ex_runner(self, 'get_tree.py', args, res_checker)
+
+
+if __name__ == '__main__':
+    unittest.main()
