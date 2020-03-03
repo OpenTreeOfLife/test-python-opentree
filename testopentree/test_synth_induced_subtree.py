451551f00d73046cb1fe605d004ae3b4763e8b89
Mark T. Holder <mtholder@gmail.com>
2020-03-03 13:01:06 -0600

    more tests

---
+++ testopentree/test_synth_induced_subtree.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import unittest
+from testopentree import run_example_script_or_skip as ex_runner
+
+class TestExamples(unittest.TestCase):
+
+    def test_study_properties(self):
+
+        def res_checker(test_case, results):
+            tree = results.tree
+            tn = tree.taxon_namespace
+            for taxon in tn:
+                if taxon.label != 'mrcaott246ott1566':
+                    test_case.assertEqual(199350, taxon.ott_id)
+
+        args = ['--ott-ids=199350', '--node-ids=mrcaott246ott1566'] # ott733093']
+        ex_runner(self, 'synth_induced_subtree.py', args, res_checker)
+
+
+if __name__ == '__main__':
+    unittest.main()
